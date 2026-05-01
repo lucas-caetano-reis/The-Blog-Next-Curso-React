a47cdc4e34c42f0677228fa13fea49c03f9c3452
Lucas <lucas.caetano.reis@hotmail.com>
2026-04-30 21:06:46 -0300

    Server Components vs Client Components com "use client"

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,11 @@
-import clsx from 'clsx';
+import { Header } from '@/components/Header';
 
-export default function HomePage() {
+export default async function HomePage() {
+  console.log('HomePage')
+
   return (
     <div>
-      <h1
-        className={clsx(
-          'text-6xl',
-          'font-bold',
-          'text-blue-500',
-          'hover:text-blue-50',
-          'hover:bg-blue-500',
-          'transition',
-          'duration-300',
-        )}
-      >
-        Olá de dentro da page.tsx
-      </h1>
+      <Header />
     </div>
   );
 }
